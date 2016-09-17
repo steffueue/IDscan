597409be0c95e4db0d9f0451d40c1fbada2b6de4
Stephanie Stroka <stephanie.stroka@digitalid.net>
2016-09-17 13:28:47 +0200

    The bitmap is now created from an image that was taken with the camera.

--- app/src/main/java/ch/hackzurich/idscan/OpenPhotoActivity.java
+++ app/src/main/java/ch/hackzurich/idscan/OpenPhotoActivity.java
@@ -1,24 +1,24 @@
 package ch.hackzurich.idscan;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -27,78 +27,86 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import ch.hackzurich.idscan.config.Configuration;
-import org.json.JSONException;
-import org.json.JSONObject;
 
+// TODO: fix the rotate issue (save file path on onPause and create file again onCreate)
 public class OpenPhotoActivity extends AppCompatActivity {
     
-    private static final String TAG = Configuration.class.getSimpleName();
+    private static final String TAG = OpenPhotoActivity.class.getSimpleName();
     
     private static final int REQUEST_IMAGE_CAPTURE = 1;
+    
+    /**
+     * The file which stores the photo that we just took.
+     */
+    private File photoFile;
+    
+    /**
+     * Create a unique file name.
+     */
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+            imageFileName,  /* prefix */
+            ".jpg",         /* suffix */
+            storageDir      /* directory */
+        );
+        photoFile = image;
 
-    private static String capturedPhotoFilePath;
-    
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-    
-        File tempFile = null;
-        try {
-            tempFile = File.createTempFile("idscan", ".jpg");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        capturedPhotoFilePath = tempFile.getAbsolutePath();
-        Uri capturedPhotoUri = Uri.fromFile(tempFile);
-        
-        takePictureIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, capturedPhotoUri);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
+        return image;
     }
     
-    protected void sendImageToOCRService(byte[] imageBytes) {
-        try {
-            URL url = new URL(Configuration.getConfigValue(this, "microsoft.ocr.api.url"));
-            HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-    
-            String contentType = "application/octet-stream";
-    
-            httpConn.setDoOutput(true);
-            httpConn.setRequestMethod("POST");
-            httpConn.setRequestProperty("Accept", "*/*");
-            httpConn.setRequestProperty("Content-type", contentType);
-            httpConn.setRequestProperty("Ocp-Apim-Subscription-Key", Configuration.getConfigValue(this, "microsoft.ocr.api.key"));
-            OutputStream outputStream = httpConn.getOutputStream();
-            outputStream.write(imageBytes);
-            outputStream.close();
-            
-            Log.d(TAG, "Response Code: " + httpConn.getResponseCode());
-            Log.d(TAG, "Response Body: " + httpConn.getResponseMessage());
-            
-            final BufferedReader streamReader = new BufferedReader(new InputStreamReader(httpConn.getInputStream(), "UTF-8")); 
-            StringBuilder responseStrBuilder = new StringBuilder();
-
-            String inputStr;
-            while ((inputStr = streamReader.readLine()) != null) {
-                responseStrBuilder.append(inputStr);
+    /**
+     * Takes a picture and stores it in the external storage.
+     */
+    private void dispatchTakePictureIntent() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            if (!Settings.System.canWrite(this)) {
+//                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//                        Manifest.permission.READ_EXTERNAL_STORAGE}, 2909);
+//            }
+//        }
+        
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        
+            // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e(TAG, ex.getMessage(), ex);
             }
-            Log.d(TAG, "Received: " + responseStrBuilder.toString());
-            final JSONObject jsonObject = new JSONObject(responseStrBuilder.toString());
-        } catch (MalformedURLException e) {
-            Log.e(TAG, e.getMessage(), e);
-        } catch (ProtocolException e) {
-            Log.e(TAG, e.getMessage(), e);
-        } catch (IOException e) {
-            Log.e(TAG, e.getMessage(), e);
-        } catch (JSONException e) {
-            Log.e(TAG, e.getMessage(), e);
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                                                      "ch.hackzurich.idscan.fileprovider",
+                                                      photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
     }
     
     @Override
     public void onSaveInstanceState(Bundle bundle) {
         super.onSaveInstanceState(bundle);
-        bundle.putString("fileName", capturedPhotoFilePath);
+    }
+    
+    /**
+     * Returns the bitmap from the taken photo or null in case the file was not found.
+     */
+    private Bitmap createBitMap() {
+        try {
+            return BitmapFactory.decodeStream(new FileInputStream(photoFile));
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "Failed to find image: " + e.getMessage(), e);
+        }
+        return null;
     }
     
     @Override
@@ -107,23 +115,18 @@ public class OpenPhotoActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE) {
             // Make sure the request was successful
             if (resultCode == RESULT_OK) {
-                Bitmap bitmap = BitmapFactory.decodeFile(capturedPhotoFilePath, new BitmapFactory.Options());
-                
                 ImageView imageView = (ImageView) findViewById(R.id.photo);
+                Bitmap bitmap = createBitMap();
                 imageView.setImageBitmap(bitmap);
                 
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-                byte[] byteArray = stream.toByteArray();
-                
-                new AsyncTask<byte[], Void, Void>() {
-    
-                    @Override 
-                    protected Void doInBackground(byte[]... bytes) {
-                        sendImageToOCRService(bytes[0]);
-                        return null;
-                    }
-                }.execute(byteArray);
+//                new AsyncTask<byte[], Void, Void>() {
+//    
+//                    @Override 
+//                    protected Void doInBackground(byte[]... bytes) {
+//                        sendImageToOCRService(bytes[0]);
+//                        return null;
+//                    }
+//                }.execute(byteArray);
             }
         }
     }
@@ -134,10 +137,6 @@ public class OpenPhotoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_open_photo);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        
-        if (savedInstanceState != null) {
-            capturedPhotoFilePath = savedInstanceState.getString("fileName");
-        }
         
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
